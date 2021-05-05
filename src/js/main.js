f7e7c29b7f73c2d9387bcb8b2125b8b9da454068
Georgy <marakhovskij@gmail.com>
2021-05-05 14:10:39 +0300

    Dropdown menu.

--- src/js/main.js
+++ src/js/main.js
@@ -12,3 +12,15 @@ $('button').eq(2).on('click', () => {
   $('.w-500').fadeToggle(800);
 });
 
+$('.wrap').html(`
+  <div class="dropdown">
+    <button class="btn btn-primary dropdown-toggle" id="dropdownMenuBtn">Dropdown button</button>
+    <ul class="dropdown-menu" data-toggle-id="dropdownMenuBtn">
+      <li><a class="dropdown-item" href="#">Action 1</a></li>
+      <li><a class="dropdown-item" href="#">Action 2</a></li>
+      <li><a class="dropdown-item" href="#">Action 3</a></li>
+    </ul>
+  </div>`
+);
+
+$('.dropdown-toggle').dropdown();
